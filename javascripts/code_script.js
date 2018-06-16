cdd6738fccf1b0c7530a2492fb5aa8c13f606fc2
Azamat Sarkytbayev <a.sarkytbayev@gmail.com>
2018-06-15 18:52:13 -0700

    edited code samples page

--- javascripts/code_script.js
+++ javascripts/code_script.js
@@ -1,4 +1,16 @@
 function main() {
   alert("Here's an annoying alert! Haha");
+  $('.cproject').hide();
+  $('.ccode-button').on('click', function() {
+    $(this).toggleClass('active');
+    if ($(this).text() === 'Show C Code') {
+      $(this).text('Viewing C Code');
+      $('.cproject').fadeIn(1000);
+    }
+    else {
+      $(this).text('Show C Code');
+      $('.cproject').fadeOut(1000);
+    };
+  });
 }
 $(document).ready(main);
